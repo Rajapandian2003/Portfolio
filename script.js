1ac81d4060cd1214491adc800aabf523534c2290
Rajapandian <161719716+Rajapandian2003@users.noreply.github.com>
2025-04-20 18:51:32 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function netflix(){
-    window.location.href="https://github.com/Rajapandian2003/Netflix_Clone";
-}
-
-function diabetes(){
-    window.location.href="https://github.com/Rajapandian2003/Diabetes_Assistance";
-}
